f17d9effdbf8da0f987d628dd185810b5534155b
Soham Kolhatkar <sohamk130405@gmail.com>
2024-12-21 15:36:33 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="flex justify-center items-center w-full h-dvh">
+      <h1 className="text-xl">Hello world</h1>
+    </div>
+  );
+};
+
+export default Home;
