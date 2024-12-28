a27e7b1790eaa84e6a1c2938efd8861bb04e1524
om-surushe <omsurushe1907@gmail.com>
2024-12-28 19:38:42 +0530

    Refactor middleware imports and remove unused user actions file

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,5 @@
 import { getToken } from "next-auth/jwt";
-import { NextResponse } from "next/server";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request });
