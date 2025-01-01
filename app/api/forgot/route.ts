ceda234d5749d75bfb0f6561ba62e5477f7990d1
om-baji <ombaji124@gmail.com>
2025-01-01 11:38:28 +0530

    change in smtp config

--- app/api/forgot/route.ts
+++ app/api/forgot/route.ts
@@ -5,8 +5,7 @@ import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT),
+  service : "gmail",
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASSWORD,
