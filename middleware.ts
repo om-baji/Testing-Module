8cda17ca9a105ae4dc1a3478e3119c31e96d79b2
om-baji <ombaji124@gmail.com>
2025-01-22 22:26:34 +0530

    debug log

--- middleware.ts
+++ middleware.ts
@@ -6,6 +6,10 @@ export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request });
   const url = request.nextUrl;
 
+  console.log({
+    token
+  })
+
   if (
     url.pathname.startsWith("/api-docs") ||
     url.pathname.startsWith("/api/swagger")
