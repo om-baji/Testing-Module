7c67387ef2a1c06ebce0753180e3e71135982e39
om-baji <ombaji124@gmail.com>
2025-01-22 22:40:51 +0530

    swagger fixes

--- middleware.ts
+++ middleware.ts
@@ -6,10 +6,6 @@ export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request });
   const url = request.nextUrl;
 
-  console.log({
-    token
-  })
-
   if (
     url.pathname.startsWith("/api-docs") ||
     url.pathname.startsWith("/api/swagger")
